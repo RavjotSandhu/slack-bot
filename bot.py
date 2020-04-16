7c8a1a16c60099a73025892d2314738a46db023d
RavjotSandhu <58950644+RavjotSandhu@users.noreply.github.com>
2020-04-15 23:39:57 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+
+import os
+from slack import WebClient
+from slack.errors import SlackApiError
+import GetOldTweets3 as got
+import datetime
+
+
+if __name__ == "__main__":
+ slack_token = os.environ["SLACK_BOT_TOKEN"]
+ client = WebClient(token=slack_token)
+
+ tweetCriteria = got.manager.TweetCriteria().setUsername("elonmusk")\
+                                           .setMaxTweets(10)
+tweet = got.manager.TweetManager.getTweets(tweetCriteria)[0]
+
+ try:
+  response = client.chat_postMessage(
+    channel="#random",
+    text=tweet.text
+    text= tweet.date 
+   )
+ except SlackApiError as e:
+  assert e.response["error"]  # str like 'invalid_auth', 'channel_not_found'
